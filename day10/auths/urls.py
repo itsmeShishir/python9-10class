4b90658f51965876e87d54f47777923d97b9521a
itsmeShishir <91515501+itsmeShishir@users.noreply.github.com>
2025-01-28 09:51:52 +0545

    just a basic one

--- day10/auths/urls.py
+++ day10/auths/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import UserList, UserRegister
+from .views import LoginApi, Profile, UpdateProfile, UserList, UserRegister, changePassword,  logout_user
 from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('users/', UserList.as_view() , name="users" ),
     path('register/', UserRegister.as_view() , name="register" ),
+    path('login', LoginApi.as_view() , name="login" ),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('profile', Profile.as_view() , name="profile" ),
+    path('updateprofile', UpdateProfile.as_view() , name="profile" ),
+    path("changepassword/", changePassword.as_view() , name="changepassword" ),
+    path("logout/", logout_user.as_view() , name="logout" ),
 ]
--- day10/auths/views.py
+++ day10/auths/views.py
@@ -3,6 +3,10 @@ from .serializations import UserSerializer, UserRegisterSerializer
 from .models import User
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate, update_session_auth_hash
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import permissions
 
 # Create your views here.
 class UserList(generics.ListAPIView):
@@ -15,6 +19,90 @@ class UserRegister(generics.CreateAPIView):
 
     
 # from rest_framework_simplejwt.tokens import RefreshToken
+class LoginApi(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user = authenticate(email=email, password=password)
 
-# token = RefreshToken(base64_encoded_token_string)
-# token.blacklist()
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        if not user:
+            return Response({'message': 'Crediancial donenot match'})
+
+        if user is not None:
+           update_session_auth_hash(request, user)
+           return Response({
+            'status': 200,
+            'access': access_token,
+            'refresh': str(refresh),
+            'message': 'Login Success', 
+            'username': user.username, 
+            "role": user.role,
+            "email": user.email
+            })
+        else:
+           return Response({'message': 'Login Failed'})
+           
+#class Profile section  -> token send in header -> using access token
+
+class Profile(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+    
+class changePassword(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserRegisterSerializer
+
+    def get_object(self):
+        return self.request.user
+    
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(
+                serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=400)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': 200,
+                'message': 'Password updated successfully',
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=400)
+
+
+#update profile 
+class UpdateProfile(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user   
+
+
+#class logout 
+class logout_user(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request):
+        token = request.data.get('token')
+        try:
+            RefreshToken(token).blacklist()
+            return Response({'message': 'Logout Success'})
+        except Exception as e:
+            return Response({'message': 'Logout Failed'})
